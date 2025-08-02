c93f2b4b725c33358d1732ac7d99dbe431ad06e6
DanielAngul0 <daniel.andres.angulo.perez@gmail.com>
2025-08-01 20:57:25 -0500

    Avance del servidor

--- 01_sockets/servidor.py
+++ 01_sockets/servidor.py
@@ -1,1 +1,19 @@
-# Servidor
+# Importando Sockets
+import socket
+
+HOST = 'localhost'
+PORT = 9000
+
+# Creando el servidor/canal de comunicacion
+servidor = socket.socket(socket.AF_INET, socket.SOCK:STERAM)
+servidor.bind((HOST, PORT))
+
+# El servidor esta en modo escucha
+servidor.listen
+print("El servidor esta a la espera de conexiones...")
+
+cliente, direccion = servidor.accept()
+print(f"Un cliente se conecto desde la {direccion}")
+
+# Recibiendo los datos
+datos = cliente.recv(1024)
