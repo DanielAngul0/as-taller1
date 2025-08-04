582f84757f9aebeab7689dd512500c0b0f7f82c6
DanielAngul0 <daniel.andres.angulo.perez@gmail.com>
2025-08-03 22:21:47 -0500

    El servidor ahora lee y responde a informacion enviada por el cliente

--- 01_sockets/servidor.py
+++ 01_sockets/servidor.py
@@ -1,19 +1,30 @@
-# Importando Sockets
+# Importando la libreria Sockets
 import socket
 
-HOST = 'localhost'
-PORT = 9000
+# Definiendo la maquina HOST
+HOST = 'localhost' # IP
+# Elegimos un puerto superior a 1024 pues hasta este puerto todos estan reservados
+PORT = 9000 # Puerto
 
 # Creando el servidor/canal de comunicacion
-servidor = socket.socket(socket.AF_INET, socket.SOCK:STERAM)
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Con esto asociamos nuestro servidor a la IP: 'localhost' y al puerto: '9000'
 servidor.bind((HOST, PORT))
 
-# El servidor esta en modo escucha
-servidor.listen
+# El servidor esta a la escucha
+servidor.listen()
 print("El servidor esta a la espera de conexiones...")
 
-cliente, direccion = servidor.accept()
+# Si un cliente se conecta, poder saber quien es el cliente y cual es su direccion
+cliente, direccion = servidor.accept() # Con 'servidor.accept()' espera las conexiones 
 print(f"Un cliente se conecto desde la {direccion}")
 
-# Recibiendo los datos
-datos = cliente.recv(1024)
+# Recibiendo los datos del cliente
+# El Buffer es una memoria temporal en donde almacena los datos antes de leerlos o enviarlos
+datos = cliente.recv(1024) # Esto leera hasta 1024 bytes / 1 kilobyte, los textos simples en inglés (ASCII) ocupan 1 byte por carácter
+
+# Respondiendo al cliente
+# Los sockets trabajan con binarios, así que todo lo que se envía o recibe debe ser codificado/decodificadoe
+cliente.sendall(b"Hola! " + datos) # Los datos que se mandan atrevés de un Socket deben ser binario, no String
+# Cierra la conexion con el cliente
+cliente.close()
